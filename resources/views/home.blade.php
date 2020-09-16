f3bada4d2a49db5e4c610cc793e48b45660dd088
tranngoctan16t5 <tranngoctan16t5@gmail.com>
2020-09-16 09:55:39 +0700

    client

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container-fluild">
+    <div class="row">
+
+
+   {{--  <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -18,6 +21,113 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+    <aside class="sidebar col-md-2">
+   <div class="sidebar-container">
+      <div class="sidebar-header">
+         <div class="brand">
+            <img alt="Sun" width="80" src="/packs/media/images/sun-logo-d67769668be9305fe08f1119c1ad8897.png">
+            <span>Education</span>
+         </div>
+      </div>
+      <nav class="menu">
+         <ul class="sidebar-menu metismenu" id="sidebar-menu">
+            <li id="sidebar-member">
+               <a href="">
+               <i class="fa fa-list-alt"></i>
+               Khóa học của tôi
+               </a>
+            </li>
+            <li id="sidebar-member">
+               <a href="">
+               <i class="fa fa-bar-chart"></i>
+               Lịch sử báo cáo
+               </a>
+            </li>
+            <li id="sidebar-member">
+               <a href="">
+               <i class="fa fa-calendar-o"></i>
+               Báo cáo hằng ngày
+               </a>
+            </li>
+         </ul>
+      </nav>
+  </div>
+</aside>
+<div  class="col-md-10 bg-success ">
+    <div class="card card-info">
+  <div class="card-header">
+    <div class="header-block">
+      <p class="title">Báo cáo hằng ngày</p>
     </div>
+  </div>
+  <div class="card-block">
+    <section class="mod-course">
+      <div class="container">
+
+<form action="/vi/reports" accept-charset="UTF-8" method="post"><input type="hidden" name="authenticity_token" value="J5wdyrT83kNuI6zinRJL3w0ZwyvsB6318VF5TCkc4S0424ab+RFXyqxOPaa50yznZlXsaOxWNHuh55dLGHU+GA==">
+  <div class="form-group">
+    <label for="report_Chọn khóa học">Chọn khóa học</label>
+    <div class="row">
+      <div class="col-sm-12">
+         <select class="form-control" name="report[course_id]" id="report_course_id"><option value="26">[ĐN-OE36 + Practice5] PHP</option></select>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="report_Chọn ngày">Chọn ngày</label>
+    <div class="row">
+      <div class="col-sm-12">
+         <input class="form-control" type="date" name="report[created_at]" id="report_created_at">
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="label--required" for="report_work_today">Bạn đã làm những gì vào hôm nay?</label>
+    <div class="row">
+      <div class="col-sm-12">
+         <textarea class="form-control" name="report[work_today]" id="report_work_today"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="report_problem">Gặp phải những vấn đề nào không?</label>
+    <div class="row">
+      <div class="col-sm-12">
+         <textarea class="form-control" name="report[problem]" id="report_problem"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label class="label--required" for="report_work_tomorrow">Dự định của bạn vào ngày mai là gì?</label>
+    <div class="row">
+      <div class="col-sm-12">
+         <textarea class="form-control" name="report[work_tomorrow]" id="report_work_tomorrow"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <button name="button" type="submit" class="btn btn-primary">Tạo Báo Cáo</button>
+</form>
+      </div>
+    </section>
+  </div>
+</div>
+
+
+</div>
+<footer class="footer">
+  <div class="footer-block buttons">
+     2019 Ⓒ <strong>Sun Asterisk</strong>. All rights reserved.
+ </div>
+ <div class="footer-block author">
+    Version: <strong>2.0.0</strong>
+</div>
+</footer>
+</div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{--  <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,10 +19,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('frontends/layout.css') }}">
+    <link rel="stylesheet" href="{{ asset('admins/bower_components/font-awesome/css/font-awesome.min.css')}}">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -44,7 +46,7 @@
                             <a class="nav-link" href="{{ route('language.index',['vi']) }}">VI</a>
                         </li>
 
-                          <li class="nav-item">
+                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('language.index',['en']) }}">EN</a>
                         </li>
                         @guest
@@ -80,12 +82,12 @@
         </div>
     </nav>
 
-    <main class="py-4">
+    <main>
         @yield('content')
     </main>
 </div>
-{{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> --}}
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
